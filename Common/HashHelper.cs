d222f90e92c46ffe438f7c08fe49da15cee80eba
MihailUse <alkaevmih@gmail.com>
2022-11-04 14:08:21 +0300

    add HashHelper in Common project

---
+++ Common/HashHelper.cs
@@ -0,0 +1,10 @@
+﻿using Isopoh.Cryptography.Argon2;
+
+namespace Common
+{
+    public class HashHelper
+    {
+        public static string GetHash(string value) => Argon2.Hash(value, parallelism: Environment.ProcessorCount);
+        public static bool Verify(string hash, string value) => Argon2.Verify(hash, value);
+    }
+}
